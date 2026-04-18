6e1add7208a533e5ece398066aae0795f3c7eb34
thurrzfl <arthurpaulo1979@gmail.com>
2026-04-18 15:33:22 -0300

    ajeitei um erro de digitação

--- EXEMPLO-CURSO/atividade-da-faculdade.cs
+++ EXEMPLO-CURSO/atividade-da-faculdade.cs
@@ -3,25 +3,11 @@ namespace AtiviadeDaFaculdade;
 class Progam
 {
     static void Main()
-    {
-        int quantidade;
-        Console.WriteLine("Oque você deseja: arroz, feijão ou macarrão? Digite o número correspondente: '1' para arroz, '2' para feijão e '3' para macarrão");
-        string opcao = Console.ReadLine();
-        switch (opcao)
+    {     foreach (var item in carro)
         {
-            case "1" : Console.WriteLine("Quantos kg ou fardos de arroz você deseja?");
-            quantidade = int.Parse(Console.ReadLine());
-            Console.WriteLine("O valor total é: " + (quantidade * 5.00) + "reais");
-            break;
-            case "2" : Console.WriteLine("Quantos kg de feijão você deseja?");
-            quantidade = int.Parse(Console.ReadLine());
-            Console.WriteLine("O valor total é: " + (quantidade * 7.00) + " reais");
-            break;
-            case "3" : Console.WriteLine("Quantos kg de macarrão você deseja?");
-            quantidade = int.Parse(Console.ReadLine());
-            Console.WriteLine("O valor total é: " + (quantidade * 4.00) + " reais");
-            break;
+            Console.WriteLine(item);
         }
     }
-}
 
+}
+  
